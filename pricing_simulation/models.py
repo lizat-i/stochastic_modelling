1c8a63c8d18dda0031643c80c263818a76cb6526
ivan lizat <ivan.lizat@outlook.de>
2024-10-11 14:03:38 +0200

    removed trinomial trees, will posibly added later

--- pricing_simulation/models.py
+++ pricing_simulation/models.py
@@ -10,7 +10,7 @@ def brownian_motion_with_mean_reversion(array: np.array):
 
     int this model spot prices mean reverts to the long term level S_hat = exp(mu) with a speed given by the mean reversion rate alphs
     the mean reversion rate can and is being derived by historical data.
-    
+
     Parameters:
     - mu: Drift coefficient, representing the average expected return of the asset.
     - sigma: Volatility of the asset, representing the randomness in price changes.
@@ -21,44 +21,6 @@ def brownian_motion_with_mean_reversion(array: np.array):
     
 
 
-    Returns:
-    - np.array: The input array filled with simulated asset prices over time.
-    """
-    mu      = 0.01 
-    sigma   = 0.3
-    T       = 5.0
-    # Number of steps derived from the length of the array
-    steps = len(array)
-    
-    # Calculate the time increment for each step
-    dt = T / steps
-    
-    # Set the initial value of the asset price
-    array[0] = 100  # You can change this to your desired starting price (e.g., S0)
-
-    # Generate the random shocks (Wiener process increments)
-    random_shocks = np.random.normal(0, 1, steps - 1)
-    
-    # Iterate through each time step to calculate the price using the GBM formula
-    for i in range(1, steps):
-        # Calculate the change in the price
-        dS = (mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * random_shocks[i - 1]
-        
-        # Update the price based on the previous price and the calculated change
-        array[i] = array[i - 1] * np.exp(dS)
-    
-    return array
-
-def trinomial_tree(array: np.array):
-    """
-    Simulates a Geometric Brownian Motion (GBM) path using an input array.
-    
-    Parameters:
-    - array: An array of zeros with length equal to the number of steps.
-    - mu: Drift coefficient, representing the average expected return of the asset.
-    - sigma: Volatility of the asset, representing the randomness in price changes.
-    - T: Total time duration (e.g., in years).
-    
     Returns:
     - np.array: The input array filled with simulated asset prices over time.
     """
